5c97fbfbb1d44caaa052c72714b7cdcb76e34b28
mina2508 <mina.kameel.nashed@gmail.com>
2022-05-06 00:05:54 +0200

    adding footer

--- library_frontend/src/App.js
+++ library_frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components.js/Footer';
 // import { Routers, Router } from 'react-router-dom';
 import NavComponent from './components.js/NavComponent';
 function App() {
   return (
     <div className="App">
       <NavComponent />
+      <Footer />
     </div>
   );
 }
