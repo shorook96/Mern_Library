1ec2719f27aee87a6740238fabe903bdc601b154
mina2508 <mina.kameel.nashed@gmail.com>
2022-05-06 20:26:22 +0200

    change something

--- library_frontend/src/components/BeforeLogging.js
+++ library_frontend/src/components/BeforeLogging.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 
 const BeforeLogging = () => {
   return (
     <div className="w-50 m-auto  ">
-      <button className="btn btn-outline-warning btn-success btn-lg w-25 m-2 btn_logging">
+      <button className="btn btn-outline-warning btn-success btn-rounded  m-2 btn_logging">
         SignUp
       </button>
-      <button className="btn btn-outline-warning btn-success btn-lg w-25 m-2  btn_logging">
+      <button className="btn btn-outline-warning btn-success btn-rounded  m-2  btn_logging">
         LogIn
       </button>
     </div>
