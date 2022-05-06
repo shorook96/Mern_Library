18aaec8a97a9eccb30d67fa7ccb8994944193cf5
mina2508 <mina.kameel.nashed@gmail.com>
2022-05-06 20:13:18 +0200

    finishing navabar

---
+++ library_frontend/src/components/BeforeLogging.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const BeforeLogging = () => {
+  return (
+    <div className="w-50 m-auto  ">
+      <button className="btn btn-outline-warning btn-success btn-lg w-25 m-2 btn_logging">
+        SignUp
+      </button>
+      <button className="btn btn-outline-warning btn-success btn-lg w-25 m-2  btn_logging">
+        LogIn
+      </button>
+    </div>
+  );
+};
+
+export default BeforeLogging;
