5636e44aeff8e8a4fcc08f82a2f0a1e181c0c62d
mina2508 <mina.kameel.nashed@gmail.com>
2022-05-13 17:59:39 +0200

    editing backend

--- library_backend/models/User/userSchema.js
+++ library_backend/models/User/userSchema.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
-    firstname: 'string',
-    lastname: 'string',
-    email: 'string', 
-    password: 'string',
-    books: [{
+  firstname: 'string',
+  lastname: 'string',
+  email: 'string',
+  password: 'string',
+  books: [
+    {
+      book: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'author',
-      }],
+        ref: 'book',
+      },
+      state: {
+        type: 'string',
+        enum: ['Read', 'Want To Read', 'CurrentlyReading'],
+      },
+    },
+  ],
 });
 
 module.exports = userSchema;
--- library_frontend/src/components/LogIn.js
+++ library_frontend/src/components/LogIn.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import { Form, Modal, Button } from 'react-bootstrap';
-import { Formik,Field,ErrorMessage,FieldArray,FastField, useFormik} from 'formik'
-
-
-const initialValues ={
-  email:"",
-  password:""
-}
-
-const onSubmit = values =>{
-  console.log(values)
-}
-
-const validate =  values =>{
-  let errors = {}
-
-  if (!values.email){errors.email = 'required'}
+import {
+  Formik,
+  Field,
+  ErrorMessage,
+  FieldArray,
+  FastField,
+  useFormik,
+} from 'formik';
+const initialValues = {
+  email: '',
+  password: '',
+};
+const onSubmit = (values) => {
+  console.log(values);
+};
+const validate = (values) => {
+  let errors = {};
+  if (!values.email) {
+    errors.email = 'required';
+  }
   // else if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i.test(values.email)){
   //     errors.email = "invalid email formate"
   // }
-  if (!values.password){errors.password = 'required'}
-  return errors
-  
-}
+  if (!values.password) {
+    errors.password = 'required';
+  }
+  return errors;
+};
 const LogIn = ({ clicked, handleLogInClose }) => {
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate
-  })
+    validate,
+  });
   // console.log(formik.values)
   // console.log(formik.errors)
   return (
@@ -39,34 +44,51 @@ const LogIn = ({ clicked, handleLogInClose }) => {
       <Modal.Body>
         <Form onSubmit={formik.handleSubmit}>
           <Form.Group className="mb-3">
-
-            <Form.Label htmlFor='email'>Email address</Form.Label>
-            <Form.Control type="email" id='email' name='email' onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur} placeholder="name@example.com" />
-            {formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
-
+            <Form.Label htmlFor="email">Email address</Form.Label>
+            <Form.Control
+              type="email"
+              id="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              onBlur={formik.handleBlur}
+              placeholder="name@example.com"
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <div className="error">{formik.errors.email}</div>
+            ) : null}
             <Form.Label>Email address</Form.Label>
             <Form.Control
               type="email"
               placeholder="name@example.com"
               autoFocus
             />
-
           </Form.Group>
           <Form.Group className="mb-3">
-            <Form.Label htmlFor='password'>Password</Form.Label>
-            <Form.Control type="password" id='password' name='password' onChange={formik.handleChange} value={formik.values.password} onBlur={formik.handleBlur} placeholder="" />
-            {formik.touched.password && formik.errors.password ? <div className='error'>{formik.errors.password}</div> : null}
-                
+            <Form.Label htmlFor="password">Password</Form.Label>
+            <Form.Control
+              type="password"
+              id="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              onBlur={formik.handleBlur}
+              placeholder=""
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <div className="error">{formik.errors.password}</div>
+            ) : null}
           </Form.Group>
-          <Button type='submit' variant="primary" onClick={handleLogInClose}>
-          LogIn
-        </Button>
+          <Button type="submit" variant="primary" onClick={handleLogInClose}>
+            LogIn
+          </Button>
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleLogInClose}>
           Cancel
         </Button>
+
         {/* <Button  variant="primary" onClick={handleLogInClose}>
           LogIn
         </Button> */}
@@ -74,5 +96,4 @@ const LogIn = ({ clicked, handleLogInClose }) => {
     </Modal>
   );
 };
-
 export default LogIn;
