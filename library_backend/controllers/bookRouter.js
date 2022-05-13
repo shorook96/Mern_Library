ee0c5ce37d8e1f8a0bead5f92703b01eb95ac8bc
Islam Kortam <eng.islam.kortam@gmail.com>
2022-05-14 00:16:13 +0200

    Done with book POST and PATCH requests

--- library_backend/controllers/bookRouter.js
+++ library_backend/controllers/bookRouter.js
@@ -1,2 +1,40 @@
 const express = require('express');
 const bookRouter = express.Router();
+
+const bookModel = require('../models/bookModel');
+const customError = require('../utils/customError');
+
+
+
+
+
+bookRouter.post('/', async (req, res, next) => {
+    const bookData = req.body;
+    console.log(bookData);
+    try{
+        await bookModel.create(bookData);
+        console.log("Success");
+        res.send({success: 'book created successfully'});
+        return;
+    }catch(error){
+        next(error);
+    }
+})
+
+bookRouter.patch('/:id', async (req, res, next) => {
+    let _id = req.params.id;
+    const newBookData = req.body;
+    try{
+        const exists = await bookModel.findById({_id});
+        if(!exists){
+            throw customError(400, 'NOT_FOUND', 'No such Book');
+        }
+        await bookModel.findByIdAndUpdate({_id}, newBookData);
+        res.send({success: 'book updated successfully'});
+    }catch(error){
+        next(error);
+    }
+})
+
+
+module.exports = bookRouter;
--- library_backend/server.js
+++ library_backend/server.js
@@ -4,6 +4,8 @@ require('./db');
 const categoryRouter = require('./controllers/categoryRouter');
 const {errorHandling_middleware} = require('./middle_wares/errorHandling_middleware');
 
+const bookRouter = require('./controllers/bookRouter');
+
 const app = express();
 const cors = require('cors')
 const port = 5000;
@@ -12,21 +14,27 @@ const userRouter = require('./models/User/userRouter');
 app.use(cors()) 
 app.use(express.json()); 
    
-app.use('/user', userRouter)    
+app.use('/user', userRouter);
 
 
 
-app.use((err, req, res, next) => {
+
+
+
+/* app.use((err, req, res, next) => {
   if (!err.status) {
     res.status(522).send({ message: 'something went wrong' });
     return next();
   }
   res.status(err.status).send({ message: err.errorMsg });
   return next();
-});
+}); */
+app.use('/books', bookRouter);
 
 app.use('/admin',adminRouter);
 app.use('/categories', categoryRouter);
+
+
 
 app.use(errorHandling_middleware);
 
