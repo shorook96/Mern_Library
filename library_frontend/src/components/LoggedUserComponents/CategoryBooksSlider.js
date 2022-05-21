0fcc60f86f4bba2ad82995e0a72f4447d0567e54
mina2508 <mina.kameel.nashed@gmail.com>
2022-05-21 23:07:48 +0200

    pushing categories

---
+++ library_frontend/src/components/LoggedUserComponents/CategoryBooksSlider.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import PaginationComponent from './PaginationComponent';
+import BookItemComponent from './BookItemComponent';
+import { UseAuth } from '../Helpers/Auth';
+import { Container } from 'react-bootstrap';
+
+const CategoryBooksSlider = ({ categoryId }) => {
+  const [res, setRes] = useState({});
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const { user } = UseAuth();
+
+  const changeCurrent = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:5000/user/${user.userInfo.id}/books/${currentPage}`,
+        {
+          headers: {
+            authorization: user.userInfo.authorization,
+          },
+        }
+      )
+      .then((response) => {
+        setRes(response.data);
+      })
+      .catch((error) => {
+        console.log('errrrrrrrrrrrrrrrrrrrrrrr ' + error);
+      });
+  }, [currentPage]);
+
+  return (
+    <>
+      {res.books && res.booksCount ? (
+        <Container>
+          <PaginationComponent
+            Data={res.books}
+            itemsCount={Number(res.booksCount)}
+            totalPageCount={Math.ceil(Number(res.booksCount) / 6)}
+            RenderComponent={BookItemComponent}
+            changeCurrent={changeCurrent}
+            currentPageNumber={currentPage}
+            title="All Books"
+          />
+        </Container>
+      ) : (
+        // <h1>Data</h1>
+        <h1>loading...</h1>
+      )}
+    </>
+  );
+};
+
+export default CategoryBooksSlider;
